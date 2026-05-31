c6d5d3933041b2afb158303a8529b2a9e93b608a
Young-ho Lee <nsa05070@icloud.com>
2026-05-31 17:47:34 +1000

    update to Google Apps Script

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ============================================================
 // ★ API 주소 — 스프레드시트 바뀌면 여기만 수정하세요!
 // ============================================================
-const API_URL = 'https://script.google.com/macros/s/AKfycbyBVwWEb9ryRyU8Tlrqm_CTyXJIz9RTZty6rR0M_H-KHNG1wcwEkns_PoSeMgXr_WA/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzR1r20jxNqCB573n57ytf9Dbo-Kb7vrmLCeIH9sS9JtNt83uNHaT0sfMzCPRHiaOw/exec';
