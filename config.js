82512efc7f7796cd361f09bc08d2f94d071ac58a
Young-ho Lee <nsa05070@icloud.com>
2026-06-05 13:58:20 +1000

    Modification due to client request

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ============================================================
 // ★ API 주소 — 스프레드시트 바뀌면 여기만 수정하세요!
 // ============================================================
-const API_URL = 'https://script.google.com/macros/s/AKfycby-v9H3DYFYCXVl6UX6MFD8ID2nspegDX3FGLH9jo2TpwuPTLXx9xTcp07fkyaJNpw/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbw4tL-5zqZVsrXfajDHREBBaGuyiVSJjY7cUR-nrATOxhCeQ_cV5JGZpXzAvzLFaRw/exec';
